a55df55949708c837ed234f0eb9a438a90556719
Selfsta <david.rico5@gmail.com>
2023-02-01 12:17:43 -0800

    Tuesday Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Slider from './Components/Slider/Slider';
 import Questionaire from './Containers/QuestionaireContainer';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className='overlay2'></div>
       <div className='content'>
         <h1>BrainLog</h1>
+        <Slider/>
         <Questionaire/>
       </div>
     </div>
---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,10 @@
+function Slider() {
+    return (
+        <div>
+            <img src='Faces.png' alt=''/>
+            <p>I will be a Slider</p>
+        </div>
+    );
+}
+
+export default Slider;
